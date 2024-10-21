919e8ea1fb74de8d50b347c2b6cad292edd6418e
mohammadaljaber <mohammadaljaber091@gmail.com>
2024-10-21 18:16:17 +0300

    install admin dashboard

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -46,4 +48,11 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    protected function name():Attribute{
+        return Attribute::make(
+            get:fn($value,$attributes)=>$attributes['first_name']
+        );
+    }
+
 }
